6ca23d03f8f460c0008fdbc146c14adb96e07c35
kenichiy19 <kenichiy058@gmail.com>
2026-03-16 09:53:11 -0300

    Aula 02: Variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis-desafio.py
@@ -0,0 +1,20 @@
+# DESAFIO 01
+
+nome = input("Qual é o seu nome: ")
+print(f"Seja bem-vindo, {nome}!")
+
+# DESAFIO 02
+
+dia = input("Qual é o seu dia de aniversário?: ")
+mes = input("Qual é o seu mês de aniversário?: ")
+ano = input("Qual é o seu ano de aniversário?: ")
+
+print(f"O seu aniversário é {dia}/{mes}/{ano}, {nome}.")
+
+# DESAFIO 03
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+soma = numero1 + numero2
+
+print(f"A soma entre os dois número é: {soma}, {nome}.")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,24 @@
+print("Olá, mundo!")
+print(7+4)
+print("7"+"4") # concatenando strings
+
+# comentários em python: utiliza-se #
+
+'''
+Comentarios de 
+multiplas linhas com tres aspas
+'''
+
+nome = "Kenichi"
+idade = 18
+peso = 80
+
+print(nome, idade, peso)
+
+# input() - utilizado para receber dados do usuário
+
+nome = input("Digite o seu nome: ")
+idade = input("Digite a sua idade: ")
+peso = input("Digite o seu peso: ")
+
+print(nome, idade, peso)
